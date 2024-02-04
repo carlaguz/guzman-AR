a187688b6b36c5e3350fa0afda315fe0fc1c789d
carly <ceciliacgrx@gmail.com>
2024-02-03 23:36:40 -0600

    agregando ServerExpress

---
+++ Parcial1/ServerExpress/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Respondiendo a peticion get desde server express =)');
+})
+
+app.post('/'), (req, res) => {
+    res.send('Respondiendo a peticion post desde server express =)')
+}
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port} =)`)
+})

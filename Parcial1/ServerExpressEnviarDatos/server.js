4f1c36086759911815434d2909365c3c76007302
carly <ceciliacgrx@gmail.com>
2024-02-09 09:48:50 -0600

    recibir formulario, express urlencoded

--- Parcial1/ServerExpressEnviarDatos/server.js
+++ Parcial1/ServerExpressEnviarDatos/server.js
@@ -7,6 +7,7 @@ const port = 3000;
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.text());
+app.use(express.urlencoded({extended: false}));
 
 app.use((req, res, next)=>{
     console.log("hi")
@@ -15,24 +16,30 @@ app.use((req, res, next)=>{
 
 // Envio de parametros en las peticiones. Revision el dia de mañana.
 // Enviar parametros en la query string
-app.get('/alumnos', (req, res, next) => {
+app.get('/personajes', (req, res, next) => {
     console.log(req.query);
     res.send('Respondiendo a peticion get desde server express =)');
 })
 
 // Recibiendo parametros como parte de la ruta
-app.get('/maestros/:carrera', (req, res, next) => {
+app.get('/personajes/:franquicia', (req, res, next) => {
     console.log(req.params.carrera);
     res.send('Respondiendo a peticion get desde server express =)');
 })
 
 // Recibiendo parametros como json en el body
-app.get('/administrativos', (req, res, next) => {
+app.get('/titulos', (req, res, next) => {
     // console.log(req.body);
 
     for (const campo in req.body) {
         console.log(req.body[campo]);
     }
+    res.send('Respondiendo a peticion get desde server express =)');
+})
+
+// Recibir un formulario
+app.get('/videogames', (req, res, next) => {
+    console.log(req.body);
     res.send('Respondiendo a peticion get desde server express =)');
 })
 

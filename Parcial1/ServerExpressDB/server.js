4251346eb6c8a5b72accc66911520cd094d01258
carly <ceciliacgrx@gmail.com>
2024-02-21 09:53:36 -0600

    modificando tarea serverexpressdb

--- Parcial1/ServerExpressDB/server.js
+++ Parcial1/ServerExpressDB/server.js
@@ -23,50 +23,36 @@ app.get('/videojuego', (req, res) => {
     if (typeof req.query.IdVideojuego == 'undefined') {
       connection.query(
         'SELECT * FROM videojuego', function (err, results) {
-          if (err) {
-            console.error("no work");
-            res.status(500).send("no data fetched");
-            return;
-          }
-          if (results.length == 0) {
-            res.status(404).send("no games fetched");
-          }
-          else {
-            console.log(results);
-            res.send(results);
-          }
+
+          (results.length == 0) ? res.status(404).send("no games fetched") : console.log(results); res.send(results)
+
         });
     }
-
     else {
     connection.query(
-      'SELECT * FROM videojuego WHERE IdVideojuego = ' + req.query.IdVideojuego, 
+      'SELECT * FROM videojuego WHERE IdVideojuego = ' + req.query.IdVideojuego,
       function (err, results) {
-        if (err) {
-          console.error("no work");
-          res.status(500).send("no data fetched");
-          return;
-        }
-        if (results.length == 0) {
-          res.status(404).send("no games fetched");
-        }
-        else {
-          console.log(results);
-          res.send(results);
-        }
-      }
-    )}
-    // Mostrar cuando no hay ningun dato
-    // res.status(400).json("")
+        
+        (results.length == 0) ? res.status(404).send("no games fetched") : console.log(results); res.send(results);
+  
+      });
+    }
 })
 
 // POST
 app.post('/videojuego', (req, res) => {
   console.log(req.body);
-  res.send(req.body);
+  //res.send(req.body);
   // Insert
-  // affectedRows
+  
 })
+
+// DELETE
+
+
+// PUT/PATCH
+// PUT -> 
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port} =)`)

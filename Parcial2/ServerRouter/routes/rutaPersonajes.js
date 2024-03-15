19b7be72b48ac027447beadccde3164aae4de5d6
Carla Guzman <ceciliacgrx@gmail.com>
2024-03-14 23:47:19 -0500

    actualizando tareass

--- Parcial2/ServerRouter/routes/rutaPersonajes.js
+++ Parcial2/ServerRouter/routes/rutaPersonajes.js
@@ -1,20 +1,92 @@
 const express = require('express');
-const rutapersonajes = express.Router;
+const router = express.Router();
+const mysql = require('mysql2');
 
-rutapersonajes.get('/', (req, res, next) => {
-    res.send('GET request')
+app.use(express.json());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'sonicAPI',
+    port: '3306',
+    password: 't33nt1t4n5'
+  });
+
+// GET
+router.get('/characters', (req, res) => {
+
+    console.log(req.query.Id)
+
+    if (typeof req.query.Id == 'undefined') {
+      connection.query(
+        'SELECT * FROM Characters', function (err, results) {
+
+          (results.length == 0) ? res.status(404).send("no chars fetched") : console.log(results); res.send(results)
+
+        });
+    }
+    else {
+    connection.query(
+      'SELECT * FROM Characters WHERE Id = ' + req.query.Id,
+      function (err, results) {
+        
+        (results.length == 0) ? res.status(404).send("no chars fetched") : console.log(results); res.send(results);
+  
+      });
+    }
 })
 
-rutapersonajes.post('/', (req, res, next) => {
-    res.send('POST request')
+// POST
+router.post('/characters', (req, res) => {
+  try {
+    const { Nombre, Especie, Genero, Alineamiento, Habilidad } = req.body;
+    console.log(req.body);
+    if(!Nombre || !Especie || !Genero || !Alineamiento || !Habilidad){
+      res.status(400).send("missing fields!");
+    }
+    connection.query(
+      'INSERT INTO `Characters` (`Nombre`, `Especie`, `Genero`, `Alineamiento`, `Habilidad`) VALUES (?, ?, ?, ?, ?)',
+      [Nombre, Especie, Genero, Alineamiento, Habilidad],
+      function (err, results) { 
+        console.log(results);
+        res.status(202).send("character created successfully");
+      })
+  } catch(err) {
+    res.status(500).send(err.message)
+  }
 })
 
-rutapersonajes.put('/', (req, res, next) => {
-    res.send('PUT request')
+// DELETE
+router.delete('/characters/:Id', (req, res) => {
+  try {
+    const { Id } = req.params;
+    connection.query(
+      'DELETE FROM `Characters` WHERE `Id` = ?', [Id],
+      function (err, results) {
+        console.log(results); 
+        res.status(202).send("char deleted successfully");
+      })
+    } catch(err) {
+      res.status(500).send(err.message);
+    }
 })
 
-rutapersonajes.delete('/', (req, res, next) => {
-    res.send('DELETE request')
+// PUT
+router.put('/characters/:Id', (req, res) => {
+  try {
+    const { Id } = req.params;
+    const { Nombre, Especie, Genero, Alineamiento, Habilidad } = req.body;
+    console.log(req.body);
+    connection.query(
+      'UPDATE `Characters` SET `Nombre` = ?, `Especie` = ?, `Genero` = ?, `Alineamiento` = ?, `Habilidad` = ? WHERE `Id` = ? ', 
+      [Nombre, Especie, Genero, Alineamiento, Habilidad, Id],
+      function (err, results) {
+        console.log(results);
+        res.status(202).send("char updated successfully");
+      })
+    } catch(err) {
+      res.status(500).send(err.message)
+    }
 })
 
-module.exports.rutapersonajes = rutapersonajes();
+module.exports = router();
--- Parcial2/ServerRouter/server.js
+++ Parcial2/ServerRouter/server.js
@@ -3,11 +3,9 @@ const { rutapersonajes } = require('./routes/rutaPersonajes');
 const app = express();
 const port = 8080;
 
-app.use('/personajes', rutapersonajes);
+app.use(express.json());
 
-app.use((err, req, res, next) => {
-    res.status(500).json({ error: err.message });
-  });
+app.use('/', rutapersonajes);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port} =)`)
